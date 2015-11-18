80bad824eef0e98ae47b45a00df41bdf09dbc58f
Joel Quiles <quilesbaker@gmail.com>
2015-11-17 20:17:48 -0500

    changed welcome msg

--- app/scripts/helpers/clear-nodes.js
+++ app/scripts/helpers/clear-nodes.js
@@ -7,15 +7,24 @@
  */
 
 module.exports = function() {
+
   var nodes = document.querySelectorAll('circle:not(.ring)');
+
   if(nodes && nodes.length) {
+
     for(var i in nodes) {
       if(nodes.hasOwnProperty(i)) {
         // TODO: remove click listener. just in case
         // nodes[i].removeEventListener('click');
         // remove node from parent svg -> g element
-        nodes[i].parentNode.removeChild(nodes[i]);
+        try{
+          nodes[i].parentNode.removeChild(nodes[i]);
+        } catch(e) {
+          console.error('Unable to remove child nodes.', e);
+        }
       }
     }
+
   }
+
 }
