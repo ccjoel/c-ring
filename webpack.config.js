d89551e393b5418d30cbca794662b9201b2de072
Joel Quiles <quilesbaker@gmail.com>
2021-11-18 13:05:23 -0500

    lets try again

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     entry: "./app/scripts/main.js",
     mode: "development", // "production" for prod build
     output: {
-        path: "./www/scripts",
+        path: "www/scripts",
         filename: "bundle.js"
     }
 };
